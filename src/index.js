bab53b706f0c526aaaef0974bf1106bb5b801357
Nayan <nayancoding@gmail.com>
2022-04-05 21:08:06 +0530

    confits added

--- src/index.js
+++ src/index.js
@@ -26,7 +26,17 @@ const computer_play =()=>{
     else if(computer <7) return 2;
     else return 3;
 }
+// winner celebration video
+let confit = document.getElementById("confit_outer");
+let confit_vdo = document.getElementById("confit_vdo");
 
+// looser celebration video
+let confit2 = document.getElementById("confit_outer2");
+let confit_vdo2 = document.getElementById("confit_vdo2");
+
+// Draw celebration video
+let confit3 = document.getElementById("confit_outer3");
+let confit_vdo3 = document.getElementById("confit_vdo3");
 
 
 document.getElementById("score").innerText = player_score;
@@ -35,33 +45,61 @@ const game_starts=(index)=>{
     index += 1;
     let computer = computer_play();
     if(index == computer) {
-        alert("Draw");
+        confit3.style.display = "block";
+        confit_vdo3.play();
+        setTimeout(() => {
+            confit3.style.display = "none";
+        }, 2300);
     }
     else if(index == 1)
             if(computer == 2){
-                alert("You loss");
+                confit2.style.display = "block";
+                confit_vdo2.play();
+                setTimeout(() => {
+                    confit2.style.display = "none";
+                }, 2300);
                 player_score = 0;
             }
             else{
-                alert("you win");
+                confit.style.display = "block";
+                confit_vdo.play();
+                setTimeout(() => {
+                    confit.style.display = "none";
+                }, 2500);
                 ++player_score;
             }
     else if(index == 2)
             if(computer == 3){
-                alert("You loss");
+                confit2.style.display = "block";
+                confit_vdo2.play();
+                setTimeout(() => {
+                    confit2.style.display = "none";
+                }, 2300);
                 player_score = 0;
             }
             else{
-                alert("you win");
+                confit.style.display = "block";
+                confit_vdo.play();
+                setTimeout(() => {
+                    confit.style.display = "none";
+                }, 2500);
                 ++player_score;
             }
     else if(index == 3)
             if(computer == 1){
-                alert("You loss");
+                confit2.style.display = "block";
+                confit_vdo2.play();
+                setTimeout(() => {
+                    confit2.style.display = "none";
+                }, 2300);
                 player_score = 0;
             }
             else{
-                alert("you win");
+                confit.style.display = "block";
+                confit_vdo.play();
+                setTimeout(() => {
+                    confit.style.display = "none";
+                }, 2500);
                 ++player_score;
             }
     document.getElementById("score").innerText = player_score;
